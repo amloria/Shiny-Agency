e02620b36342797925ea7ced65ee9a7b7ced8cce
Agustin Loria <agustin.loria93@gmail.com>
2022-08-04 19:14:58 +0200

    react-training

---
+++ src/index.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from './pages/Home';
+import Survey from './pages/Survey';
+import Header from './components/Header'
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Header />
+      <Route path="/">
+        <Home />
+      </Route>
+      <Route path="/survey">
+        <Survey />
+      </Route>
+    </Router>
+  </React.StrictMode>
+);
